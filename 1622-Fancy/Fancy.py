916fe2684f552ff80b8aa00bec7d5ce8f548b5ac
Nacer Sebti <nacersebti.ms@gmail.com>
2022-01-23 19:40:18 -0500

    Fancy

---
+++ 1622-Fancy/Fancy.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 14:57:23 2022
+
+@author: nacer
+"""
+
+"""
+Problem link : https://leetcode.com/problems/fancy-sequence/
+"""
+
+class Fancy:
+
+    def __init__(self):
+        self.L = []
+        self.M = []
+        self.A = []
+
+
+    def append(self, val):
+        self.L.append(val)
+        if len(self.M) == 0:
+            self.M.append(1)
+            self.A.append(0)
+        else:
+            self.M.append(self.M[-1])
+            self.A.append(self.A[-1])
+        
+
+    def addAll(self, inc):
+        if len(self.A) > 0:
+            self.A[-1] += inc
+        
+
+    def multAll(self, m):
+        if len(self.M) > 0:
+            self.M[-1] *= m
+            self.A[-1] *= m
+
+
+    def getIndex(self, idx):
+        
+        n = len(self.L)
+        
+        if idx >= n:
+            return -1
+        if idx == 0:
+            inc = self.A[-1]
+            m = self.M[-1]
+
+        else:
+            inc = self.A[-1] - self.A[idx-1] * (self.M[-1]//self.M[idx-1])
+            m = self.M[-1]//self.M[idx-1]
+
+        return ( self.L[idx] * m + inc ) % (10**9 + 7)
+
+
+# Your Fancy object will be instantiated and called as such:
+# obj = Fancy()
+# obj.append(val)
+# obj.addAll(inc)
+# obj.multAll(m)
+# param_4 = obj.getIndex(idx)
