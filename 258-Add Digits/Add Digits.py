b8971ce0208018e131bef679af4f5f3754ac6eea
Nacer Sebti <nacersebti.ms@gmail.com>
2022-02-08 11:11:04 -0500

    Add Digits O(1)

--- 258-Add Digits/Add Digits.py
+++ 258-Add Digits/Add Digits.py
@@ -11,9 +11,21 @@ Problem link : https://leetcode.com/problems/add-digits/
 
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num <= 9:
-            return num
-        s = 0
-        for digit in str(num):
-            s += int(digit)
-        return self.addDigits(s)
+        output = 0
+        while num != 0:
+            digit = num % 10
+            num = num // 10
+            if output + digit >= 10:
+                output = output + digit - 9
+            else:
+                output += digit
+        return output
+   
+
+def addDigitsRecursion(num):
+    if num <= 9:
+        return num
+    s = 0
+    for digit in str(num):
+        s += int(digit)
+    return addDigitsRecursion(s)
